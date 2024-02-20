72acec8791175075fa42c44ed02bb09f2ae169d4
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-20 17:38:30 +0530

    read JSON

--- src/functions/animeGet.js
+++ src/functions/animeGet.js
@@ -1,11 +1,11 @@
 const fs = require('node:fs');
 const db = __dirname.replace('functions', '') + 'db/anidb.json';
+const { readJSON } = require('../utils/readJSON.js');
 
 async function getAnime(anime) {
   const an = anime?.trim()?.toLowerCase();
 
-  const anidb = fs.readFileSync(db);
-  const data = JSON.parse(anidb);
+  const data = readJSON(db);
   
   for (const a of data) {
     if (a.title.toLowerCase() === an) {
