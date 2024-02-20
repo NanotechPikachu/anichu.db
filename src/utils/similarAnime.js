33e2550d9ca287a7d00a077a5a4f531d1706388b
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-20 17:36:29 +0530

    JSON read require

--- src/utils/similarAnime.js
+++ src/utils/similarAnime.js
@@ -1,9 +1,10 @@
 const fs = require('node:fs');
 const { isSimilar } = require('./isSimilar.js');
+const { readJSON } = require('./readJSON.js');
 
 function similarAnime(anime, db, ratio = 0.4) {
     let results = [];
-    const anidb = JSON.parse(fs.readFileSync(db));
+    const anidb = readJSON(db);
   
     for (let entry of anidb) {   
       if (isSimilar(entry.title?.toLowerCase(), anime) > ratio) {
