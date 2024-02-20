d47ee448f4d2b36ab3e8814739b8b15b89292244
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-02-20 20:06:11 +0530

    Asynchronous error fix

--- src/utils/similarAnime.js
+++ src/utils/similarAnime.js
@@ -2,9 +2,9 @@ const fs = require('node:fs');
 const { isSimilar } = require('./isSimilar.js');
 const { readJSON } = require('./readJSON.js');
 
-function similarAnime(anime, db, ratio = 0.4) {
+async function similarAnime(anime, db, ratio = 0.4) {
     let results = [];
-    const anidb = readJSON(db);
+    const anidb = await readJSON(db);
   
     for (let entry of anidb) {   
       if (isSimilar(entry.title?.toLowerCase(), anime) >= ratio) {
